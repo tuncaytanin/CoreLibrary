52940124fda50298d071b645a434d4c8b56e34dd
tuncay tanin <53979656+tuncaytanin@users.noreply.github.com>
2022-05-30 01:57:12 +0300

    Ders sonu

---
+++ RateLimit.API/Controllers/ProductController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RateLimit.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetProduct()
+        {
+            return Ok(new { Id = 1, Name = "Kalem", Price = 20 });
+        }
+
+        //api/porduct/kalem/20
+        [HttpGet("{name}")]
+        public IActionResult GetProdcut(string name)
+        {
+            return Ok(name);
+        }
+
+        //api/porduct/kalem/20
+        [HttpGet("{name}/{price}")]
+        public IActionResult GetProdcut(string name, int price)
+        {
+            return Ok(name +" "+price.ToString());
+        }
+
+        [HttpPost]
+        public IActionResult SavePorduct()
+        {
+            return Ok(new { Status = "Success" });
+        }
+        [HttpPut]
+        public IActionResult UpdateProuct()
+        {
+            return Ok();
+        }
+    }
+}
